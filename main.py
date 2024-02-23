2ca2aba5755193dddab4e2b31894480d02cd959b
tomas <tomasarmento@gmail.com>
2024-02-23 23:05:06 +0000

    representention of the problem done

--- main.py
+++ main.py
@@ -1,49 +1,72 @@
 
-import Library
+from Library import Library
+from Book import Book
+from sortedcontainers import SortedSet
 class DataManager:    
     def __init__(self,filename):
-        self.filename = filename
+        self.filename = "./data/" + filename
+        self.libraries = {}
         self.books = {}
-        file1 = open(filename, 'r')
+        file1 = open(self.filename, 'r')
         line = file1.readline()
-        self.read_data_info(line)
+        self.read_first_line_info(line)
         line = file1.readline()
-        self.read_data_info(line)
+        self.read_books_rating(line)
         line = file1.readline()
         libraryId = 0
-        while line:
-            data = line.split(' ')
-            nbooks = int(data[0])
-            signTime = int(data[1])
-            canShipBooksPerDay = int(data[2])
-            line = file1.readline()
-            data = line.split(' ')
-            books = {}
-            i = 0
-            for rating in data:
-                books[i] = rating
-                i += 1
-            if i != nbooks:
-                print("Error in reading book rating\nNumber of books read is not equal to the number of books mentioned in the file rating")
-            self.books[libraryId] = Library(nbooks,signTime,canShipBooksPerDay,books)
+        while line != "\n":
+            line2 = file1.readline()
+            self.read_library_data(libraryId,line,line2)
             libraryId += 1
             line = file1.readline()
-            
+        if libraryId != self.nLibraries:
+            print("Number of libraries read is not equal to the number of libraries mentioned in the file rating")
+        file1.close()
             
             
 
-    def read_data_info(self,line):
+    def read_first_line_info(self,line):
         data = line.split(' ')
         self.nBooks = int(data[0])
         self.nLibraries = int(data[1])
         self.nDays = int(data[2])
-    def read_book_rating(self,line):
+
+    def read_books_rating(self,line):
         data = line.split(' ')
-        self.books = {}
         i = 0
         for rating in data:
-            self.books[i] = rating
+            self.books[i] = int(rating)
             i += 1
         if i != self.nBooks:
             print("Error in reading book rating\nNumber of books read is not equal to the number of books mentioned in the file rating")
+
+    def read_library_data(self,libraryId,firstLine,secondLine):
+        data = firstLine.split(' ')
+        nbooks = int(data[0])
+        signTime = int(data[1])
+        canShipBooksPerDay = int(data[2])
+        bookIds = secondLine.split(' ')
+        books = self.books
+        LibraryBooks = SortedSet()
+        i = 0
+        for bookid in bookIds:# pode se alterar o ordered set para lista la a frente é so necessario ordenar mas para ja vamos tentar assim
+            LibraryBooks.add(Book(self.books[int(bookid)],int(bookid)))
+            i += 1
+        self.libraries[libraryId] = Library(nbooks,signTime,canShipBooksPerDay,LibraryBooks)
+        if i != nbooks:
+            print("Number of books from library read is not equal to the number of books mentioned in the file rating")
+    def print_libraries(self):
+        for library in self.libraries:
+            print("Library: " + str(library))
+            print("Books: ")
+            for book in self.libraries[library].books:
+                print("id:" + str(book.book_id) + " rating:" + str(book.rating))
+            print("Sign Time: " + str(self.libraries[library].signTime))
+            print("Books per day: " + str(self.libraries[library].canShipBooksPerDay))
+            print("Number of books: " + str(self.libraries[library].nbooks))
+            print("\n")
+
+if __name__ == "__main__":
+    manager =DataManager("f_libraries_of_the_world.txt")
+    manager.print_libraries()
             
